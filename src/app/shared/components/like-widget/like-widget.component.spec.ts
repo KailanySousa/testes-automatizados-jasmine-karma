97f8d9c8b18ab3424a384a6f9859647b74ce82b6
KailanySousa <kailany-sousa2010@hotmail.com>
2024-03-20 17:23:50 -0300

    test: emitir evento liked ao clicar no like

--- src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -5,6 +5,8 @@ import { LikeWidgetModule } from "./like-widget.module";
 describe(`${LikeWidgetComponent.name}`, () => {
 
     let fixture: ComponentFixture<LikeWidgetComponent> = null;
+    let component: LikeWidgetComponent = null;
+
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             imports: [LikeWidgetModule]
@@ -13,10 +15,10 @@ describe(`${LikeWidgetComponent.name}`, () => {
         }).compileComponents();
 
         fixture = TestBed.createComponent(LikeWidgetComponent);
+        component = fixture.componentInstance;
     });
 
     it(`Should create component`, () => {
-        const component = fixture.componentInstance;
         expect(component).toBeTruthy();
     });
 
@@ -28,10 +30,17 @@ describe(`${LikeWidgetComponent.name}`, () => {
     });
     
     it(`Should not generate ID when id input property is present`, () => {
-        const component = fixture.componentInstance;
         const someId = 'someId';
         component.id = someId;
         fixture.detectChanges();
         expect(component.id).toBe(someId);
+    });
+
+    it(`${LikeWidgetComponent.prototype.like.name}
+        should trigger emission when called`, () => {
+        spyOn(component.liked, 'emit');
+        fixture.detectChanges();
+        component.like()
+        expect(component.liked.emit).toHaveBeenCalled();
     });
 })
